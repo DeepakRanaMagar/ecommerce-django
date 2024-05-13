438ae52c76be0df3b51161ff6688c7e55d0f08de
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-05-13 22:11:56 +0545

    .

--- products/admin.py
+++ products/admin.py
@@ -4,17 +4,20 @@ from .models import Catalog, SubCatalog, Product
 @admin.register(Catalog)
 class CatalogAdmin(admin.ModelAdmin):
     list_display = ['name']
-
+    search_fields = ['name']
+    
 @admin.register(SubCatalog)
 class SubCatalogAdmin(admin.ModelAdmin):
     list_display = ['Catalog','name']
-
+    search_fields = ['Catalog','name']
     def Catalog(self, obj):
         return obj.Catalog.name
     
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["Category","name", "price"]
+    search_fields = ["Category","name", "price"]
 
     def Category(self, obj):
         return obj.SubCatalog.name
+    
