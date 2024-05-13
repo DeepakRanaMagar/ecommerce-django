dcf9d573be310ce3efd7037b9e12ff60b2b9f317
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-05-13 10:51:22 +0545

    .

--- core/models.py
+++ core/models.py
@@ -7,7 +7,9 @@ class Product(models.Model):
     category = models.CharField(_("category"), max_length=50)
     description = models.CharField(_("Description"), max_length=50)
     price = models.DecimalField(_("price"), max_digits=6, decimal_places=2)
-    image = models.ImageField(_("image"), upload_to='media/images/products', height_field=1080, width_field=1080, max_length=None)
+    image_width = models.IntegerField(blank=True, null=True)
+    image_height = models.IntegerField(blank=True, null=True)
+    image = models.ImageField(_("image"), upload_to='images/products/', height_field='image_height', width_field='image_width')
 
     def __str__(self):
         return self.name
