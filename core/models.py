567fdf8b1937bf2374c2f038a9c046ec029f74a9
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-05-13 10:43:26 +0545

    product

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(_("Name"), max_length=50)
+    category = models.CharField(_("category"), max_length=50)
+    description = models.CharField(_("Description"), max_length=50)
+    price = models.DecimalField(_("price"), max_digits=6, decimal_places=2)
+    image = models.ImageField(_("image"), upload_to='media/images/products', height_field=1080, width_field=1080, max_length=None)
+
+    def __str__(self):
+        return self.name
