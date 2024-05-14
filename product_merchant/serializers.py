9363fbe65bd530de66c31e2bb7c9d59b8269ca07
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-05-14 18:53:47 +0545

    .

---
+++ product_merchant/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from products.models import Product
+
+'''
+    Serialization of the input of the Merchant
+'''
+
+class ProductDetailSerializer(serializers.Serializer):
+    pass
--- product_merchant/views.py
+++ product_merchant/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .serializers import ProductDetailSerializer
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
-# Create your views here.
+class ProductDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        pass
