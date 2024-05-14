144fc7d771130a6aaf70434a0a012aa275e4778e
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-05-14 16:17:08 +0545

    .

--- products/views.py
+++ products/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework.views import APIView
 from .models import Catalog, SubCatalog, Product
 from .serializers import ProductSerializer
 from rest_framework.permissions import AllowAny
-
+from rest_framework.response import Response
 class ProductView(APIView):
     permission_classes = [AllowAny, ]
     
     def get(self, request):
         queryset = Product.objects.all()
         serializer = ProductSerializer(queryset, many=True)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
