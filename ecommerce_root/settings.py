0d236baa8f483b4b268815fd8c492c8b1d5ebea6
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-05-16 10:38:17 +0545

    .

--- ecommerce_root/settings.py
+++ ecommerce_root/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
+    'rest_framework.authtoken',
 
     'accounts.apps.AccountsConfig',
     'core.apps.CoreConfig',
