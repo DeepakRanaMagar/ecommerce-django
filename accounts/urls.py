71de4432740c60f8440916fc606f02db0ba0d573
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-05-16 10:35:46 +0545

    .

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import CustomerRegistrationView, MerchantRegistrationView
+from .views import CustomerRegistrationView, MerchantRegistrationView, UserLoginView, UserLogoutView
 
 urlpatterns = [
     path('register/customer/', CustomerRegistrationView.as_view(), name='CustomerRegistration'),
     path('register/merchant/', MerchantRegistrationView.as_view(), name='MerchantRegistration'),
+    path('login/', UserLoginView.as_view(), name='UserLogin'),
+    path('logout/', UserLogoutView.as_view(), name='UserLogin'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .serializers import CustomerRegistrationSerializer, MerchantRegistrationSerializer
 from django.http import JsonResponse
-
+from django.contrib.auth import login, authenticate
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+
 
 '''
     Registration Section
@@ -57,4 +61,38 @@ class MerchantRegistrationView(APIView):
             {
                 "serializer error": serializer.error_messages
             }, status =status.HTTP_400_BAD_REQUEST
+        )
+    
+class UserLoginView(APIView):
+    permission_classes = [AllowAny, ]
+
+    def get(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            token, created = Token.objects.get_or_create(user=user)
+            return Response(
+                {
+                    "token": token.key,
+                    "user_id": user.id
+                }, status= status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {
+                    "error": "Login Failed. Invalid Credentials"
+                }, status=status.HTTP_401_UNAUTHORIZED
+            )
+
+class UserLogoutView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.auth.delete()
+        return Response(
+            {"message": "Successfully logged out"}, status=status.HTTP_200_OK
         )
