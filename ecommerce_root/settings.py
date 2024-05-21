789620d3b1f1769280c73e6be6187638eab7f432
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-05-21 21:59:43 +0545

    env vars

--- ecommerce_root/settings.py
+++ ecommerce_root/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,24 +91,24 @@ WSGI_APPLICATION = 'ecommerce_root.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'ecommerce_db',
-#         'USER': 'ecommerce_admin',
-#         'PASSWORD': 'ecommerce_admin',
-#         'HOST': '127.0.0.1',
-#         'PORT': "5432",
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("DATABASE_NAME"),
+        'USER': config("DATABASE_USER"),
+        'PASSWORD': config("DATABASE_PASSWORD"),
+        'HOST': config("DATABASE_HOST"),
+        'PORT': config("DATABASE_PORT"),
+    }
+}
 
 
 # Password validation
