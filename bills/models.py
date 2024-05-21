eea7527a5b7d6a89cdc9f93fa5512f5ab9fe97dd
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-05-21 08:12:09 +0545

    bills: Model init

--- bills/models.py
+++ bills/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from address.models import AddressField
 from accounts.models import Customer
 from cart.models import Cart
 
@@ -16,12 +15,10 @@ PAYMENT_CHOICES = {
 class Bill(models.Model):
     customer = models.ForeignKey(Customer, verbose_name=_("customer"), on_delete=models.CASCADE)
     cart = models.ForeignKey(Cart, verbose_name=_("cart"), on_delete=models.CASCADE)
-    shipping_address = AddressField(related_name='ship_address',null=True, blank=True)
-    billing_address = AddressField(related_name='bill_address',null=True, blank=True)
+    shipping_address = models.CharField(_("shipping address"), max_length=50)
+    billing_address = models.CharField(_("billling address"), max_length=50)
     has_paid = models.BooleanField(_("has paid"))
     payment_method = models.CharField(_("Payment Method"),choices=PAYMENT_CHOICES, max_length=50)
-
-
 
 
     class Meta:
