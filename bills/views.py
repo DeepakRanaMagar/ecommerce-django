35f6d550e2eb53eb862f510f582fa2387e3f4d02
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-05-21 12:02:38 +0545

    bill fixes

--- bills/views.py
+++ bills/views.py
@@ -18,25 +18,53 @@ class BillView(APIView):
         
         user = (
             request.user
-        )  # fetch the request sending user, but we need to verify whether the 'user' is customer or merchant
-        # print(user)
-        try:  # exception handling
+        )   # fetch the request sending user, but we need to verify whether the 'user' is customer or merchant
+        
+        try:    # exception handling
             is_customer = Customer.objects.get(
                 user=user
             )  # fetch the user from the Customer DB if the requested user matches
         except Customer.DoesNotExist as e:  # if the is_customer condition fails performs following code
             return Response(
-                {"Error: Only Customers are allowed to create a Cart"},
+                {"Error: Only customers are allowed to Bill the orders.1"},
                 status=status.HTTP_401_UNAUTHORIZED,
             )
-        request.data["customer"] = is_customer.id
-        # print(request.data["customer"])
-        print(request.data)
         
+        print(is_customer)
+        request.data["customer"] = is_customer.id  #overriding the request's "Customer field" with customer_id of the "Customer" table, because we verified the user is a customer of over system
+        '''
+            Fetching the cart related to Authenticated Customer, to avoid manual insertion of the cart details
+        '''
+        try:
+            cart = Cart.objects.get(
+                customer= is_customer
+            )
+        except Cart.DoesNotExist as e:
+            return Response(
+                {
+                    "Error: There is not cart for this user."
+                }, status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        # print("cart id", cart.id)
+        request.data["cart"] = cart.id  #for automation insertion of the related cart
+        
+        #serialization  
+        seriailzer = BillSerializer(data=request.data) #passing the data for serialization
 
-        '''
-            login vako customer ko cart xa ki xaina tyo ni detemine garnu paryo 
-            ani automatically, serialize huna paryo
-        
-        '''
-        seriailzer = BillSerializer(data=request.data)
+        if seriailzer.is_valid():
+            try:
+                seriailzer.save()
+                return Response(
+                    {
+                        f"{request.user.username}, Your Billing Receipt has been created. Thank you"
+                    }, status=status.HTTP_201_CREATED
+                )
+            except Exception as e:
+                return Response(str(e))
+            
+        return Response(
+            {
+                "Error: Serializer's Validation Failed"
+            }, status=status.HTTP_400_BAD_REQUEST
+        )
